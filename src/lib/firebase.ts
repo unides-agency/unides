97686502ad89ad2e29a50061afd96d4f3e75089e
kabaskill <oguzkabasakal@gmail.com>
2025-07-19 15:10:29 +0200

    fix env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+const env = import.meta.env;
+
 const firebaseConfig = {
-
-  apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
-
-  authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-
-  projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
-
-  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-
-  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-
-  appId: process.env.PUBLIC_FIREBASE_APP_ID
-
+  apiKey: env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
